4c07ca1efb9022c5866e62dcc81f03739b9d87a9
Juliana Morikoshi <julianamorikoshi@gmail.com>
2021-12-06 20:30:12 -0300

    chore(eslint,prettier): linting config

--- 02 - Dive/dive01.js
+++ 02 - Dive/dive01.js
@@ -1,72 +1,78 @@
 /**
  * --- Day 2: Dive! ---
- * 
+ *
  * --- Part One ---
- * 
+ *
  * Now, you need to figure out how to pilot this thing.
- * 
+ *
  * It seems like the submarine can take a series of commands like forward 1, down 2, or up 3:
- * 
+ *
  * forward X increases the horizontal position by X units.
  * down X increases the depth by X units.
  * up X decreases the depth by X units.
- * 
- * Note that since you're on a submarine, down and up affect your depth, and so 
+ *
+ * Note that since you're on a submarine, down and up affect your depth, and so
  * they have the opposite result of what you might expect.
- * 
- * The submarine seems to already have a planned course (your puzzle input). 
+ *
+ * The submarine seems to already have a planned course (your puzzle input).
  * You should probably figure out where it's going. For example:
- * 
+ *
  * forward 5
  * down 5
  * forward 8
  * up 3
  * down 8
  * forward 2
- * 
- * Your horizontal position and depth both start at 0. The steps above would 
+ *
+ * Your horizontal position and depth both start at 0. The steps above would
  * then modify them as follows:
- * 
+ *
  * forward 5 adds 5 to your horizontal position, a total of 5.
  * down 5 adds 5 to your depth, resulting in a value of 5.
  * forward 8 adds 8 to your horizontal position, a total of 13.
  * up 3 decreases your depth by 3, resulting in a value of 2.
  * down 8 adds 8 to your depth, resulting in a value of 10.
  * forward 2 adds 2 to your horizontal position, a total of 15.
- * 
- * After following these instructions, you would have a horizontal position of 
+ *
+ * After following these instructions, you would have a horizontal position of
  * 15 and a depth of 10. (Multiplying these together produces 150.)
- * 
- * Calculate the horizontal position and depth you would have after following 
- * the planned course. What do you get if you multiply your final horizontal 
+ *
+ * Calculate the horizontal position and depth you would have after following
+ * the planned course. What do you get if you multiply your final horizontal
  * position by your final depth?
  */
 
-const externalInput = require('./course.json');
+const externalInput = require('./course.json')
 
-const puzzleInput = [
-  ['forward', 5],
-  ['down', 5],
-  ['forward', 8],
-  ['up', 3],
-  ['down', 8],
-  ['forward', 2]
-];
+// const puzzleInput = [
+//   ['forward', 5],
+//   ['down', 5],
+//   ['forward', 8],
+//   ['up', 3],
+//   ['down', 8],
+//   ['forward', 2]
+// ];
 
-const getPosition = (course) => {
-  const position = course.reduce((acc, action) => {
-    if (action[0] === 'forward') return { ...acc, horizontal: acc.horizontal + action[1] };
-    else if (action[0] === 'down') return { ...acc, depth: acc.depth + action[1] };
-    else if (action[0] === 'up') return { ...acc, depth: acc.depth - action[1] };
-    return { ...acc };
-  }, {
-    horizontal: 0,
-    depth: 0,
-  });
+const getPosition = course => {
+  const position = course.reduce(
+    (acc, action) => {
+      if (action[0] === 'forward')
+        return { ...acc, horizontal: acc.horizontal + action[1] }
+      else if (action[0] === 'down')
+        return { ...acc, depth: acc.depth + action[1] }
+      else if (action[0] === 'up')
+        return { ...acc, depth: acc.depth - action[1] }
+      return { ...acc }
+    },
+    {
+      horizontal: 0,
+      depth: 0
+    }
+  )
 
-  console.log(`Horizontal position: ${position.horizontal}`);
-  console.log(`Depth: ${position.depth}`);
-  return position.horizontal * position.depth;
-};
+  console.log(`Horizontal position: ${position.horizontal}`)
+  console.log(`Depth: ${position.depth}`)
+  return position.horizontal * position.depth
+}
 
-console.log(`Final value: ${getPosition(externalInput)}`);
+console.log(`Final value: ${getPosition(externalInput)}`)
--- 03 - Binary Diagnostic/binaryDiagnostic02.js
+++ 03 - Binary Diagnostic/binaryDiagnostic02.js
@@ -1,130 +1,133 @@
 /**
  * Day 3: Binary Diagnostic
- * 
+ *
  * --- Part Two ---
- * 
- * Next, you should verify the life support rating, which can be determined by 
+ *
+ * Next, you should verify the life support rating, which can be determined by
  * multiplying the oxygen generator rating by the CO2 scrubber rating.
- * 
- * Both the oxygen generator rating and the CO2 scrubber rating are values that 
- * can be found in your diagnostic report - finding them is the tricky part. 
- * Both values are located using a similar process that involves filtering out 
- * values until only one remains. Before searching for either rating value, 
- * start with the full list of binary numbers from your diagnostic report and 
+ *
+ * Both the oxygen generator rating and the CO2 scrubber rating are values that
+ * can be found in your diagnostic report - finding them is the tricky part.
+ * Both values are located using a similar process that involves filtering out
+ * values until only one remains. Before searching for either rating value,
+ * start with the full list of binary numbers from your diagnostic report and
  * consider just the first bit of those numbers. Then:
- * 
- * Keep only numbers selected by the bit criteria for the type of rating value 
+ *
+ * Keep only numbers selected by the bit criteria for the type of rating value
  * for which you are searching. Discard numbers which do not match the bit criteria.
- * If you only have one number left, stop; this is the rating value for which you 
+ * If you only have one number left, stop; this is the rating value for which you
  * are searching.
  * Otherwise, repeat the process, considering the next bit to the right.
  * The bit criteria depends on which type of rating value you want to find:
- * 
- * To find oxygen generator rating, determine the most common value (0 or 1) 
- * in the current bit position, and keep only numbers with that bit in that 
- * position. If 0 and 1 are equally common, keep values with a 1 in the position 
+ *
+ * To find oxygen generator rating, determine the most common value (0 or 1)
+ * in the current bit position, and keep only numbers with that bit in that
+ * position. If 0 and 1 are equally common, keep values with a 1 in the position
  * being considered.
- * 
- * To find CO2 scrubber rating, determine the least common value (0 or 1) in 
- * the current bit position, and keep only numbers with that bit in that position. 
+ *
+ * To find CO2 scrubber rating, determine the least common value (0 or 1) in
+ * the current bit position, and keep only numbers with that bit in that position.
  * If 0 and 1 are equally common, keep values with a 0 in the position being considered.
- * 
- * For example, to determine the oxygen generator rating value using the same 
+ *
+ * For example, to determine the oxygen generator rating value using the same
  * example diagnostic report from above:
- * 
- * Start with all 12 numbers and consider only the first bit of each number. 
- * There are more 1 bits (7) than 0 bits (5), so keep only the 7 numbers with a 
+ *
+ * Start with all 12 numbers and consider only the first bit of each number.
+ * There are more 1 bits (7) than 0 bits (5), so keep only the 7 numbers with a
  * 1 in the first position: 11110, 10110, 10111, 10101, 11100, 10000, and 11001.
- * Then, consider the second bit of the 7 remaining numbers: there are more 0 
- * bits (4) than 1 bits (3), so keep only the 4 numbers with a 0 in the second 
+ * Then, consider the second bit of the 7 remaining numbers: there are more 0
+ * bits (4) than 1 bits (3), so keep only the 4 numbers with a 0 in the second
  * position: 10110, 10111, 10101, and 10000.
- * In the third position, three of the four numbers have a 1, so keep those three: 
+ * In the third position, three of the four numbers have a 1, so keep those three:
  * 10110, 10111, and 10101.
- * In the fourth position, two of the three numbers have a 1, so keep those two: 
+ * In the fourth position, two of the three numbers have a 1, so keep those two:
  * 10110 and 10111.
- * In the fifth position, there are an equal number of 0 bits and 1 bits (one each). 
+ * In the fifth position, there are an equal number of 0 bits and 1 bits (one each).
  * So, to find the oxygen generator rating, keep the number with a 1 in that position: 10111.
  * As there is only one number left, stop; the oxygen generator rating is 10111, or 23 in decimal.
- * 
+ *
  * Then, to determine the CO2 scrubber rating value from the same example above:
- * 
- * Start again with all 12 numbers and consider only the first bit of each number. 
- * There are fewer 0 bits (5) than 1 bits (7), so keep only the 5 numbers with a 
+ *
+ * Start again with all 12 numbers and consider only the first bit of each number.
+ * There are fewer 0 bits (5) than 1 bits (7), so keep only the 5 numbers with a
  * 0 in the first position: 00100, 01111, 00111, 00010, and 01010.
- * Then, consider the second bit of the 5 remaining numbers: there are fewer 1 
- * bits (2) than 0 bits (3), so keep only the 2 numbers with a 1 in the second 
+ * Then, consider the second bit of the 5 remaining numbers: there are fewer 1
+ * bits (2) than 0 bits (3), so keep only the 2 numbers with a 1 in the second
  * position: 01111 and 01010.
- * In the third position, there are an equal number of 0 bits and 1 bits (one each). 
+ * In the third position, there are an equal number of 0 bits and 1 bits (one each).
  * So, to find the CO2 scrubber rating, keep the number with a 0 in that position: 01010.
  * As there is only one number left, stop; the CO2 scrubber rating is 01010, or 10 in decimal.
- * 
- * Finally, to find the life support rating, multiply the oxygen generator rating 
+ *
+ * Finally, to find the life support rating, multiply the oxygen generator rating
  * (23) by the CO2 scrubber rating (10) to get 230.
- * 
- * Use the binary numbers in your diagnostic report to calculate the oxygen 
- * generator rating and CO2 scrubber rating, then multiply them together. 
- * What is the life support rating of the submarine? (Be sure to represent your 
+ *
+ * Use the binary numbers in your diagnostic report to calculate the oxygen
+ * generator rating and CO2 scrubber rating, then multiply them together.
+ * What is the life support rating of the submarine? (Be sure to represent your
  * answer in decimal, not binary.)
  */
 
-const externalInput = require(`./diagnosticReport.json`);
+const externalInput = require(`./diagnosticReport.json`)
 
-const puzzleInput = [
-  "00100",
-  "11110",
-  "10110",
-  "10111",
-  "10101",
-  "01111",
-  "00111",
-  "11100",
-  "10000",
-  "11001",
-  "00010",
-  "01010",
-];
+// const puzzleInput = [
+//   '00100',
+//   '11110',
+//   '10110',
+//   '10111',
+//   '10101',
+//   '01111',
+//   '00111',
+//   '11100',
+//   '10000',
+//   '11001',
+//   '00010',
+//   '01010'
+// ]
 
-const getLifeSupportRating = (report) => {
-  const binaryNumberLength = report[0].length; // Binary number with n bits
-  let oxygenGenerator = report;
-  let cO2Scrubber = report;
+const getLifeSupportRating = report => {
+  const binaryNumberLength = report[0].length // Binary number with n bits
+  let oxygenGenerator = report
+  let cO2Scrubber = report
 
   for (let index = 0; index < binaryNumberLength; index++) {
     // Get all bit value from current position
     const currentPositionSum = {
       oxygen: { 0: 0, 1: 0 },
-      co2: { 0: 0, 1: 0 },
-    };
-    oxygenGenerator.forEach((item) => {
-      currentPositionSum.oxygen[item[index]] += 1;
-    });
-    cO2Scrubber.forEach((item) => {
-      currentPositionSum.co2[item[index]] += 1;
-    });
+      co2: { 0: 0, 1: 0 }
+    }
+    oxygenGenerator.forEach(item => {
+      currentPositionSum.oxygen[item[index]] += 1
+    })
+    cO2Scrubber.forEach(item => {
+      currentPositionSum.co2[item[index]] += 1
+    })
 
     // Eliminates the unwanted ones
     if (oxygenGenerator.length > 1) {
       const maxValue = Object.keys(currentPositionSum.oxygen).reduce((a, b) => {
-        if (currentPositionSum.oxygen[a] > currentPositionSum.oxygen[b]) return a;
-        if (currentPositionSum.co2[a] == currentPositionSum.co2[b]) return 1;
-        if (currentPositionSum.oxygen[b] <= 0) return a;
-        return b;
-      });
-      oxygenGenerator = oxygenGenerator.filter((number) => number[index] == maxValue);
+        if (currentPositionSum.oxygen[a] > currentPositionSum.oxygen[b])
+          return a
+        if (currentPositionSum.co2[a] == currentPositionSum.co2[b]) return 1
+        if (currentPositionSum.oxygen[b] <= 0) return a
+        return b
+      })
+      oxygenGenerator = oxygenGenerator.filter(
+        number => number[index] == maxValue
+      )
       // console.log(`Oxygen: ${oxygenGenerator}`);
     }
 
     if (cO2Scrubber.length > 1) {
       const minValue = Object.keys(currentPositionSum.co2).reduce((a, b) => {
         if (currentPositionSum.co2[a] < currentPositionSum.co2[b]) {
-          if (currentPositionSum.co2[a] > 0) return a;
-          return b;
+          if (currentPositionSum.co2[a] > 0) return a
+          return b
         }
-        if (currentPositionSum.co2[a] == currentPositionSum.co2[b]) return 0;
-        return b;
-      });
+        if (currentPositionSum.co2[a] == currentPositionSum.co2[b]) return 0
+        return b
+      })
 
-      cO2Scrubber = cO2Scrubber.filter((number) => number[index] == minValue);
+      cO2Scrubber = cO2Scrubber.filter(number => number[index] == minValue)
       // console.log(`CO2: ${cO2Scrubber}`);
     }
   }
@@ -132,13 +135,13 @@ const getLifeSupportRating = (report) => {
   // console.log(`Oxygen: ${oxygenGenerator}`);
   // console.log(`CO2: ${cO2Scrubber}`);
 
-  const oxygenAsDecimal = parseInt(oxygenGenerator[0], 2);
-  const cO2AsDecimal = parseInt(cO2Scrubber[0], 2);
+  const oxygenAsDecimal = parseInt(oxygenGenerator[0], 2)
+  const cO2AsDecimal = parseInt(cO2Scrubber[0], 2)
 
-  console.log(`Oxygen as decimal: ${oxygenAsDecimal}`);
-  console.log(`CO2 as decimal: ${cO2AsDecimal}`);
+  console.log(`Oxygen as decimal: ${oxygenAsDecimal}`)
+  console.log(`CO2 as decimal: ${cO2AsDecimal}`)
 
-  return oxygenAsDecimal * cO2AsDecimal;
-};
+  return oxygenAsDecimal * cO2AsDecimal
+}
 
-console.log(`Life Support Rating: ${getLifeSupportRating(externalInput)}`);
+console.log(`Life Support Rating: ${getLifeSupportRating(externalInput)}`)
